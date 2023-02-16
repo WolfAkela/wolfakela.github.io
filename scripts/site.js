386f5be675ea4e55b65ec769b36b02750bae9ceb
WolfAkela <wolfakela8@hotmail>
2023-02-16 10:10:05 +0400

    JS change

--- scripts/site.js
+++ scripts/site.js
@@ -1,12 +1,11 @@
-//const baseURL = "https://html.somee.com/api";
-// const baseURL = "https://html.somee.com/api";
+const baseURL = "https://html.somee.com/api";
 
-// function getJSON(path, callback){
-//     fetch(`${baseURL}/${path}`)
-//         .then(resp => resp.json())
-//         .then(data => {
-//             callback(data);
-//         });
-// }
+function getJSON(path, callback){
+    fetch(`${baseURL}/${path}`)
+        .then(resp => resp.json())
+        .then(data => {
+            callback(data);
+        });
+}
 
-// getJSON("Data", data => {});
+getJSON("Data", data => {});
