3c45a37c3ec3391ab2db76dcc2d041ca739693a1
WolfAkela <wolfakela8@hotmail>
2023-02-23 05:50:11 +0400

    Used jQuery AJAX insted of fetch() API

--- scripts/site.js
+++ scripts/site.js
@@ -12,51 +12,19 @@ function getJSON(path, callback) {
 }
 
 function postJSON(path, data, callback) {
-    const url = `${baseURL}/${path}`;
-
-    if (isLocal) {
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: data,
-            success: data => {
-                callback && callback(data);
-            }
-        });
-
-        return;
-    }
-
-    fetch(url, {
-        method: 'POST',
-        //mode: 'cors',
-        body: JSON.stringify(data),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(resp => resp.json())
-        .then(data => {
+    $.ajax({
+        url: `${baseURL}/${path}`,
+        type: "POST",
+        data: data,
+        success: data => {
             callback && callback(data);
-        });
+        }
+    });
 }
 
 function track() {
     postJSON("Track", null, data => { });
-    //postJSON("Test", { test: 1 }, data => { });
+    postJSON("Test", { test: 1 }, data => { });
 }
 
-if (isLocal) {
-    let script = document.createElement('script');
-    script.onload = function () {
-        track();
-    };
-    script.integrity = "sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=";
-    script.crossOrigin = "anonymous";
-    script.src = "https://code.jquery.com/jquery-3.6.3.min.js";
-    document.head.appendChild(script);
-}
-else {
-    track();
-}
+track();
